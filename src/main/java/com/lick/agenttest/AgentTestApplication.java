cd0d535a548335dfc56b5ef3827c49a18e4086df
lichengkai <lichengkai@dianwoba.com>
2018-08-01 16:04:29 +0800

    add bytebuddy test

--- src/main/java/com/lick/agenttest/AgentTestApplication.java
+++ src/main/java/com/lick/agenttest/AgentTestApplication.java
@@ -1,5 +1,6 @@
 package com.lick.agenttest;
 
+import com.lick.agenttest.example.ByteBuddyExample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,8 @@ public class AgentTestApplication {
 		System.out.printf("this is main\n");
 		AgentTestApplication agent = new AgentTestApplication();
 		agent.fun1();
+		ByteBuddyExample example = new ByteBuddyExample();
+		example.load();
 //		SpringApplication.run(AgentTestApplication.class, args);
 	}
 	public void fun1() {
---
+++ src/main/java/com/lick/agenttest/example/ByteBuddyExample.java
@@ -0,0 +1,13 @@
+package com.lick.agenttest.example;
+
+/**
+ * @author lichengkai
+ * 2018-08-01: 下午3:21
+ * Copyright(c) for dianwoda
+ */
+public class ByteBuddyExample {
+
+    public void load() {
+        System.out.printf("ByteBuddyExample.load() executing\n");
+    }
+}
